ca5b299bbe3819e4dc594c7d542a3c85421e3362
blackhat7911 <mbishal605@gmail.com>
2021-04-22 20:14:08 +0545

    project setup

---
+++ android/app/src/main/kotlin/com/example/katchymemes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.katchymemes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:katchymemes/screens/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Katchymemes',
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
